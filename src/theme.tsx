dc723ea9c334111477a9dbd0944c3beb3a4ddb99
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-09-18 21:21:37 +0700

    styling scrollbar

--- src/theme.tsx
+++ src/theme.tsx
@@ -17,9 +17,35 @@ const breakpoints = createBreakpoints({
   xl: '80em'
 })
 
+const scrollbarStyles = {
+  html: {
+    '--scrollbarBg': '#CFD8DC',
+    '--thumbBg': '#90A4AE'
+  },
+  'body::-webkit-scrollbar': {
+    width: '14px'
+  },
+  body: {
+    scrollbarWidth: 'thin',
+    scrollbarColor: 'var(--thumbBg) var(--scrollbarBg)'
+  },
+  'body::-webkit-scrollbar-track': {
+    background: 'var(--scrollbarBg)'
+  },
+  'body::-webkit-scrollbar-thumb': {
+    backgroundColor: 'var(--thumbBg)',
+    borderRadius: '6px',
+    border: '3px solid var(--scrollbarBg)'
+  },
+  'body::-webkit-scrollbar-thumb:hover': {
+    backgroundColor: '#818b94'
+  }
+}
+
 const theme = extendTheme({
   fonts,
-  breakpoints
+  breakpoints,
+  styles: { global: scrollbarStyles }
 })
 
 export default theme
