dc181d9efb2e0018e34644696fb62f85bcfd24c3
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-09-18 07:43:15 +0700

    fix state bug

--- src/pages/provinces/index.tsx
+++ src/pages/provinces/index.tsx
@@ -1,21 +1,16 @@
 import { NextPage } from 'next'
-import { ChangeEvent, useEffect, useMemo, useState } from 'react'
+import { ChangeEvent, useMemo, useState } from 'react'
 import { useAtom } from 'jotai'
 
 import Page from '@components/layout/page'
 import Jumbotron from '@components/provinces/jumbotron'
 import ProvinceList from '@components/provinces/province-list'
 import SearchBar from '@components/provinces/search-bar'
-import { provincesState, provinceState } from 'src/store'
+import { provincesState } from 'src/store'
 
 const ProvincesPage: NextPage = () => {
   const [provinces] = useAtom(provincesState)
-  const [, setProvince] = useAtom(provinceState)
   const [search, setSearch] = useState('')
-
-  useEffect(() => {
-    setProvince('')
-  }, [])
 
   const filteredProvinces = useMemo(() => {
     if (provinces.length === 0) return null
